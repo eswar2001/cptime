e7fa1322a04a8885d4772365c24d93ccab2de6e0
eswar2001 <teswar2001@gmail.com>
2021-01-17 00:47:22 +0530

    fixed nav issues

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import './navbar.css';
 export const Navbar = () => {
+    
     return (<>
-        <nav className="navbar sticky-top navbar-expand-lg">
+        <nav className="navbar  navbar-expand-lg">
             <div className="container">
                 <Link className="navbar-brand" to="/" >
                     <h4 >Contest<span>List</span></h4>
@@ -11,17 +13,16 @@ export const Navbar = () => {
                     <i className="fas fa-bars"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto w-100 justify-content-end">
-                        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/Atcoder">Atcoder</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/Codechef">Codechef</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/Codeforces">Codeforces</Link></li>
-                        <li className="nav-item" ><Link className="nav-link" to="/Hackerearth">Hackerearth</Link></li>
-                        <li className="nav-item "><Link className="nav-link" to="/Leetcode">Leetcode</Link></li>
-                        <li className="nav-item "><Link className="nav-link" to="/Kaggle">Kaggle</Link></li>
+                    <ul className="navbar-nav mr-auto justify-content-center" >
+                        <li className="nav-item"><Link onClick={(e) => { }} className="nav-link" to="/">Home</Link></li>
+                        <li className="nav-item"><Link onClick={(e) => { }} className="nav-link" to="/Atcoder">Atcoder</Link></li>
+                        <li className="nav-item"><Link onClick={(e) => { }} className="nav-link" to="/Codechef">Codechef</Link></li>
+                        <li className="nav-item"><Link onClick={(e) => { }} className="nav-link" to="/Codeforces">Codeforces</Link></li>
+                        <li className="nav-item" ><Link onClick={(e) => { }} className="nav-link" to="/Hackerearth">Hackerearth</Link></li>
+                        <li className="nav-item "><Link onClick={(e) => { }} className="nav-link" to="/Leetcode">Leetcode</Link></li>
+                        <li className="nav-item "><Link onClick={(e) => { }} className="nav-link" to="/Kaggle">Kaggle</Link></li>
                     </ul>
-                </div>
-            </div>
+                </div></div>
         </nav>
     </>)
 }
